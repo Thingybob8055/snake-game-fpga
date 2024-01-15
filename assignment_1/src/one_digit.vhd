5eb26feaa384173c0d039f29a866e15bb3577380
Akshay Gopinath <akshay8055.007@gmail.com>
2024-01-15 20:23:59 +0000

    add: stage 1 files of assignment 1

---
+++ assignment_1/src/one_digit.vhd
@@ -0,0 +1,60 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12/30/2023 02:42:05 PM
+-- Design Name: 
+-- Module Name: one_digit - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity one_digit is
+    Port ( digit : in STD_LOGIC_VECTOR (3 downto 0);
+           seg : out STD_LOGIC_VECTOR (6 downto 0);
+           an : out STD_LOGIC_VECTOR (3 downto 0);
+           dp : out STD_LOGIC);
+end one_digit;
+
+architecture Behavioral of one_digit is
+
+begin
+    process(digit)
+    begin
+    case digit is
+        when "0000" => seg <= "1000000"; -- 0
+        when "0001" => seg <= "1001111"; -- 1
+        when "0010" => seg <= "0100100"; -- 2
+        when "0011" => seg <= "0110000"; -- 3
+        when "0100" => seg <= "0011001"; -- 4
+        when "0101" => seg <= "0010010"; -- 5
+        when "0110" => seg <= "0000010"; -- 6
+        when "0111" => seg <= "1111000"; -- 7
+        when "1000" => seg <= "0000000"; -- 8
+        when "1001" => seg <= "0010000"; -- 9
+        when others => seg <= "1111111";
+    end case;
+    end process;
+end Behavioral;
